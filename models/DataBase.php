05978dbff82312a3a86752b81e584b53ba538892
Jose Bohorquez <bd567358546@gmail.com>
2025-01-15 07:57:57 -0500

    ajustando conexion

--- models/DataBase.php
+++ models/DataBase.php
@@ -1,38 +1,27 @@
 <?php
     
-class DataBase {
+    class DataBase{
 
-    public static function connection() {  
+        public static function connection(){  
 
-        // Configuración de la base de datos
-        $hostname = "localhost";
-        $port = "3306";
-        $database = "seller";   // ajustar (nombre BD) en qa servidor de pruebas
-        $username = "root";     // ajustar (usuario BD) en qa servidor de pruebas
-        $password = "";         // ajustar (Contraseña de BD) en qa servidor de pruebas
+            $hostname = "localhost";
 
-        try {
-            // Crear la conexión PDO
-            $pdo = new PDO("mysql:host=$hostname;port=$port;dbname=$database;charset=utf8", $username, $password);
+            $port = "3306";
 
-            // Establecer el modo de error a excepción
+            $database = "seller"; // ajustar (nombre BD) en qa servidor de pruebas
+
+            $username = "root";     // ajustar (usuario BD) en qa servidor de pruebas
+
+            $password = "S3rv1t3l4dm1n//";         // ajustar (Contraseña de BD) en qa servidor de pruebas
+
+            $pdo = new PDO("mysql:host=$hostname;port=$port;dbname=$database;charset=utf8",$username,$password);
+
             $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-            // mandar mensaje de test de conexion exitosa
-            #echo "conexion a BD exitosa";
-
-<<<<<<< HEAD
-            $password = "S3rv1t3l4dm1n//";         // ajustar (Contraseña de BD) en qa servidor de pruebas
-=======
-            // Retornar la conexión
             return $pdo;
->>>>>>> rama_local_jose
 
-        } catch (PDOException $e) {
-            // Si ocurre un error de conexión, se captura la excepción y muestra el mensaje
-            echo "Error de conexión a la base de datos: " . $e->getMessage();
-            // Se puede hacer un manejo adicional del error, como registrar el error en un archivo de log
         }
+
     }
-}
+    
 ?>

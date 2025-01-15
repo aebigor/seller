b7e7311b24b574626bd8de87c92f6219337c6179
Jose Bohorquez <bd567358546@gmail.com>
2025-01-15 10:07:19 -0500

    ajuste en landing unificado(se debe secmentar)

--- controllers/Landing.php
+++ controllers/Landing.php
@@ -10,7 +10,22 @@ class Landing
         $category2_products = $product->products_by_category(2); 
         $category3_products = $product->products_by_category(3); 
 
-        require_once "views/landing/modules/header_v2.php";  
+        /*
+        require_once "views/landing/modules/1_header.php";
+        require_once "views/landing/modules/2_nav_sup.php";
+        require_once "views/landing/modules/3_section_carousell.php";
+        require_once "views/landing/modules/4_celulares.php";  
+        require_once "views/landing/modules/5_computadores.php";
+        require_once "views/landing/modules/6_equipos_usados.php";
+        require_once "views/landing/modules/7_contact.php";
+        require_once "views/landing/modules/8_methods.php";
+        require_once "views/landing/modules/9_importante.php";
+        require_once "views/landing/modules/10_footer.php";
+        */
+
+        require_once "views/landing/modules/header_v2.php";
+
+
     }
 }
 

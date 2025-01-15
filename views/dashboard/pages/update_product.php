0d52aab5ce5b7f319bc502fd33665e6c96fdf8e4
Jose Bohorquez <bd567358546@gmail.com>
2025-01-15 11:42:45 -0500

    ajuste en vista update para poner +

--- views/dashboard/pages/update_product.php
+++ views/dashboard/pages/update_product.php
@@ -31,7 +31,7 @@
           <div class="col-12 col-md-6">
             <div class="form-group">
               <label for="producto_nombre" class="bmd-label-floating">Nombre del Producto</label>
-              <input name="product_name" type="text" pattern="[a-zA-ZáéíóúÁÉÍÓÚñÑ0-9 ]{1,100}" class="form-control" id="producto_nombre" maxlength="100" required value="<?php echo $product->get_name(); ?>">
+              <input name="product_name" type="text" pattern="[a-zA-ZáéíóúÁÉÍÓÚñÑ0-9 + ]{1,100}" class="form-control" id="producto_nombre" maxlength="100" required value="<?php echo $product->get_name(); ?>">
             </div>
           </div>
           
